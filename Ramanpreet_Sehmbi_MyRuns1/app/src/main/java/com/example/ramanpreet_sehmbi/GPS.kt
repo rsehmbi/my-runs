799759ddc92142c9342eb2bb54536c1c2bb72169
rsehmbi <rsehmbi@sfu.ca>
2022-05-28 12:31:57 -0700

    adds start fragment

---
+++ Ramanpreet_Sehmbi_MyRuns1/app/src/main/java/com/example/ramanpreet_sehmbi/GPS.kt
@@ -0,0 +1,19 @@
+package com.example.ramanpreet_sehmbi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class GPS : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gps)
+    }
+
+    fun OnButtonSave(view: View) {
+        finish()
+    }
+    fun OnButtonCancel(view: View) {
+        finish()
+    }
+}
---
+++ Ramanpreet_Sehmbi_MyRuns1/app/src/main/java/com/example/ramanpreet_sehmbi/Start.kt
@@ -0,0 +1,68 @@
+package com.example.ramanpreet_sehmbi
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+
+
+class Start : Fragment(){
+
+    lateinit var inputTypeSpinner: Spinner
+    lateinit var activityTypeSpinner: Spinner
+    lateinit var startButton: Button
+
+    var INPUTTYPE = "Manual Entry"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentStartView = inflater.inflate(R.layout.fragment_start, container, false)
+
+        inputTypeSpinner = fragmentStartView.findViewById(R.id.input_type_id)
+        val inputTypeAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.input_types, android.R.layout.simple_list_item_1);
+        inputTypeSpinner.adapter = inputTypeAdapter
+        inputTypeSpinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                INPUTTYPE = "Manual Entry"
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val type = parent?.getItemAtPosition(position).toString()
+                INPUTTYPE = type
+            }
+
+        }
+
+        activityTypeSpinner = fragmentStartView.findViewById(R.id.activity_types_id)
+        val activityTypeAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.activity_types, android.R.layout.simple_list_item_1);
+        activityTypeSpinner.adapter = activityTypeAdapter
+        activityTypeSpinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //TODO: Requirement not specified yet.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //TODO: Requirement not specified yet.
+            }
+        }
+
+        startButton = fragmentStartView.findViewById(R.id.start_button_id)
+        startButton.setOnClickListener(){
+            if(INPUTTYPE == "Manual Entry"){
+                Toast.makeText(context, "Manual Entry Page", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                startActivity(Intent(activity, GPS::class.java))
+            }
+        }
+
+
+        return fragmentStartView
+    }
+
+}
