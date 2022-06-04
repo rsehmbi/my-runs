61c5d1f32c0fb329247707e1252238e9d219330e
rsehmbi <rsehmbi@sfu.ca>
2022-06-04 14:06:51 -0700

    Adds Gallery Support

--- Ramanpreet_Sehmbi_MyRuns1/app/src/main/java/com/example/ramanpreet_sehmbi/UserProfileViewModal.kt
+++ Ramanpreet_Sehmbi_MyRuns1/app/src/main/java/com/example/ramanpreet_sehmbi/UserProfileViewModal.kt
@@ -1,10 +1,12 @@
 package com.example.ramanpreet_sehmbi
 
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UserProfileViewModal: ViewModel() {
     // Reference: Prof's class example
     val userProfileImage = MutableLiveData<Bitmap>()
+    val userProfileImage2 = MutableLiveData<Uri>()
 }
